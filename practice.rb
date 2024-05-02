295c1ea957d88591ff3c2e92efb32018a6f0988c
Xinyi Zhang <celine.zhang@kellogg.northwestern.edu>
2024-05-02 21:28:51 +0000

    answer

--- practice.rb
+++ practice.rb
@@ -39,15 +39,16 @@ blockchain = [
 
 # end
 
-ben = blockchain[0]["amount"] + blockchain[6]["amount"] - blockchain[2]["amount"] - blockchain[5]["amount"]
-puts "Ben's KelloggCoin balance is #{ben}"
+ben_balance = 0
 
-brian = blockchain[1]["amount"] - blockchain[3]["amount"] 
-evan = blockchain[2]["amount"] - blockchain[4]["amount"] + blockchain[7]["amount"] 
-anthony = blockchain[3]["amount"] + blockchain[4]["amount"] + blockchain[5]["amount"] - blockchain[6]["amount"] - blockchain[7]["amount"] 
-puts "Brian's KelloggCoin balance is #{brian}"
-puts "Evan's KelloggCoin balance is #{evan}"
-puts "Anthony's KelloggCoin balance is #{anthony}"
+
+for transaction in blockchain
+  if transaction ["from_user"] == "ben"
+    ben_balance = ben_balance - transaction["amount"]
+  end
+end
+
+puts "#{ben_balance}"
 
 
 
